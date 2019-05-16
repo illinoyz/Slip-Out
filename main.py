9a0667daf0e92a34e37067a86539f5cd35d54deb
TitaniumHocker <titaniumhocker@protonmail.com>
2019-05-17 02:30:14 +0300

    some code templates

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+import pygame
+import sys
+from collections import defaultdict
+
+
+class Game:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ mainMenu.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+import pygame
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
